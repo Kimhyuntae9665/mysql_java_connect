6f7985df21685abce8a9761d3c0d18069063e61b
rlagusxo <rlagusxo@DESKTOP-M6P2TK9>
2023-01-25 17:25:06 +0900

    MainApplication이 최종

--- mysqlConnect/src/data_base/DAO/Board_DAO.java
+++ mysqlConnect/src/data_base/DAO/Board_DAO.java
@@ -40,7 +40,16 @@ public class Board_DAO {
 			 resultSet = statement.executeQuery(SQL);
 			 
 			 while(resultSet.next()) {
+				 Integer id = resultSet.getInt(1);
+				 String boardTitle = resultSet.getString(2);
+				 String boardContent = resultSet.getString(3);
+				 String boardDateTime = resultSet.getString(4);
+				 Integer boardLike = resultSet.getInt(5);
+				 Integer boardWriter = resultSet.getInt(6);
 				 
+				 BoardEntity entity = new BoardEntity(id, boardTitle, boardContent, boardDateTime, boardLike, boardWriter);
+//				 entity.toString();
+				 result.add(entity);
 			 } 
 			 
 			 
@@ -50,6 +59,18 @@ public class Board_DAO {
 		}catch(Exception exception) {
 			
 		}finally {
+			try {
+				if(connection!=null && !connection.isClosed())
+					connection.close();
+				if(statement !=null && !statement.isClosed())
+					statement.close();
+				if(resultSet !=null && !resultSet.isClosed())
+					resultSet.close();
+				
+				
+			}catch(Exception exception) {
+				exception.printStackTrace();
+			}
 			
 		}
 		
--- mysqlConnect/src/index/MainApplication.java
+++ mysqlConnect/src/index/MainApplication.java
@@ -1,13 +1,25 @@
 package index;
 
+import java.util.List;
 
+import data_base.DAO.Board_DAO;
+import data_base.Entity.BoardEntity;
 
 public class MainApplication {
 	
 	
 
 	public static void main(String[] args) {
+//		DAO 인스턴스 생성 
+		Board_DAO dao = new Board_DAO();
+//		데이터베이스에서 Board 테이블의 전체 레코드를 검색해서 출력 
 		
+		List<BoardEntity> findResult = dao.find(); //Board_DAO에서 만든 함수 find()
+		
+		for(BoardEntity entity:findResult) {
+//			toString()이라도 return 값이 아무리 문자열 이라도 sop 출력문안에 입력해야 한다 
+			System.out.println(entity.toString());
+		}
 	}
 
 }
